7557a0df4747d2312f5cb84bb3c17af5cb130e48
Jordan <jordan.laczkowski23@gmail.com>
2022-08-17 19:37:20 -0600

    started making the 5 day forecast and connecting the api information to what was needed in each category

--- js/script.js
+++ js/script.js
@@ -1,30 +1,49 @@
-var requestUrl =
-  "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=e48f1126c6ee01bbe48f0a9ad118cdd2";
-
-var nameOfCity = "london";
+var nameOfCity = "London";
 var limit = 5;
+var lat;
+var lon;
 var getLatAndLong =
-  "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=e48f1126c6ee01bbe48f0a9ad118cdd2";
+  "http://api.openweathermap.org/geo/1.0/direct?q=" +
+  nameOfCity +
+  "&appid=e48f1126c6ee01bbe48f0a9ad118cdd2";
+//console.log(getLatAndLong);
 
-fetch(requestUrl)
+fetch(getLatAndLong)
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
+    lat = data[0].lat;
+    lon = data[0].lon;
+    getWeather(lat, lon);
   });
 
+function getWeather(lat, lon) {
+  var getWeather =
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=e48f1126c6ee01bbe48f0a9ad118cdd2";
+
+  fetch(getWeather)
+    .then((response) => response.json())
+    .then((data) => {
+      fillDays(data.dt, data.main.temp, data.wind.speed, data.main.humidity);
+      console.log(data);
+    });
+}
+
 //How are we suppose to know the lat and long of a city when someone types it in if lat and long are required in the api call
-function fillDays() {
+function fillDays(date, temp, wind, humidity) {
   $(".day").each(function () {
-    //note -- referencing this time-block -- depending on time of day it is a certain color
-    var hourBlock = $(this).attr("id");
-    if (hourBlock < currentHour) {
-      $(this).addClass("past");
-    }
-    if (hourBlock == currentHour) {
-      $(this).addClass("present");
-    } else if (hourBlock > currentHour) {
-      $(this).addClass("future");
-    }
+    $(".date").val(date);
+    $(".temperature").val(temp);
+    $(".wind").val(wind);
+    $(".humidity").val(humidity);
   });
 }
-timeCompare();
+//timeCompare();
+
+/*Questions: 
+1. How to use jQuery to fill in the date, temp, wind, and humidity for each day?
+2. How to get the current day and 5 day forecast? 
+*/
