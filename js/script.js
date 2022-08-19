53804f59248780773a8853664168569c6fb6dfd2
Jordan <jordan.laczkowski23@gmail.com>
2022-08-18 19:43:37 -0600

    got rid of lines of code that weren't needed

--- js/script.js
+++ js/script.js
@@ -2,23 +2,13 @@ var nameOfCity = "Denver";
 var limit = 5;
 var lat;
 var lon;
-//e48f1126c6ee01bbe48f0a9ad118cdd2
-//9b430ea1318141ffbb5b685ae9fb887b
+
 var apiKey = "9b430ea1318141ffbb5b685ae9fb887b";
 var getLatAndLong =
   "http://api.openweathermap.org/geo/1.0/direct?q=" +
   nameOfCity +
   "&appid=" +
   apiKey;
-//console.log(getLatAndLong);
-
-// fetch(getLatAndLong)
-//   .then((response) => response.json())
-//   .then((data) => {
-//     lat = data[0].lat;
-//     lon = data[0].lon;
-//     getWeather(lat, lon);
-//   });
 
 function getWeather(nameOfCity) {
   var getWeather =
@@ -30,38 +20,25 @@ function getWeather(nameOfCity) {
     fetch(getWeather)
       .then((response) => response.json())
       .then((data) => {
-        // fillDays(data.dt, data.main.temp, data.wind.speed, data.main.humidity);
-        // console.log(data);
-
+        $("#card-0 .date-0").val(data.data[0].datetime);
+        $("#card-0 .temperature-0").val(data.data[0].temp);
+        $("#card-0 .wind-0").val(data.data[0].wind_spd);
+        $("#card-0 .humidity-0").val(data.data[0].rh);
+        $("#card-0 .uv-index-0").val(data.data[0].uv);
         console.log(data);
-        $("#card-0 .date-0").val("soomethig");
-        // for (i = 0; i < data.data.length; i++) {
-        //   var cardId = "card-" + i;
-        //   var date = data.data[i].datetime;
-        //   var temp = data.data[i].temp;
-        //   var wind = data.data[i].wind_spd;
-        //   var humidity = data.data[i].rh;
-        //   $("date-" + "i").val(date);
-        //   $("temperature-" + "i").val(temp);
-        //   $("wind-" + "i").val(wind);
-        //   $("humidity-" + "i").val(humidity);
-        // }
+        for (i = 1; i < data.data.length; i++) {
+          var date = data.data[i].datetime;
+          var temp = data.data[i].temp;
+          var wind = data.data[i].wind_spd;
+          var humidity = data.data[i].rh;
+          $("#card-" + i + " .date-" + i).val(date);
+          $("#card-" + i + " .temperature-" + i).val(temp);
+          $("#card-" + i + " .wind-" + i).val(wind);
+          $("#card-" + i + " .humidity-" + i).val(humidity);
+        }
       });
 }
-
 getWeather(nameOfCity);
-
-//How are we suppose to know the lat and long of a city when someone types it in if lat and long are required in the api call
-function fillDays(date, temp, wind, humidity) {
-  console.log(date, temp, wind, humidity);
-
-  $(".day").each(function (date, temp, wind, humidity) {
-    date.appendTo($(this).children(".date"));
-    $(this).val(temp);
-    $(this).val(wind);
-    $(this).val(humidity);
-  });
-}
 
 $("#search-button").on("click", function (event) {
   event.preventDefault();
@@ -76,44 +53,10 @@ $("#search-button").on("click", function (event) {
       })
     )
     .then((response) => response.json())
-    .then(function (data) {
-      //MAKE SURE YOU ITERATE THE SAME NUMBER OF TIMES AS THE NUMBER OF PETS PULLED FROM API (REMEMBER ERIK)
-      for (i = 0; i < 6; i++) {
-        var weatherForecast = {
-          pictureSource: "",
-          date: "",
-          wind: "",
-          temp: "",
-          humidity: "",
-        };
-        console.log(data.animals[i]);
-        console.log(data.animals[i].primary_photo_cropped);
-        if (data.animals[i].primary_photo_cropped === null) {
-          newPet.pictureSource =
-            "https://i0.wp.com/orstx.org/wp-content/uploads/2019/10/no-photo-available-icon-12.jpg?fit=300%2C245&ssl=1"; //add path to image in folder
-        } else {
-          var object = data.animals[i].primary_photo_cropped;
-          var key = Object.keys(object)[0];
-          newPet.pictureSource = object[key];
-        }
-        newPet.name = data.animals[i].name;
-        newPet.age = data.animals[i].age;
-        newPet.sex = data.animals[i].gender;
-        array.push(newPet);
-      }
-      displayPets(array);
-    });
+    .then(function (data) {});
 });
 
-//timeCompare();
-
-/*Questions: 
-1. How to use jQuery to fill in the date, temp, wind, and humidity for each day?
-2. How to get the current day and 5 day forecast? 
-3. UV Index??? 
-*/
-
-/*create a card, display individual to display data for each day given 
-append html to each card and (look at project 1)
-if uv is ... (<2) the apply class to thing and gives background color 
+/* 
+Need API key to give back data
+HOw do I link the values using jQuery 
 */
